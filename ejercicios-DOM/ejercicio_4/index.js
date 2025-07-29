3f138079d3ed2f6d087dc9eab20b4484805fd928
Alex <alexgarciacollado1@gmail.com>
2025-07-29 19:40:43 +0200

    Ejercicios DOM listos, tarde productiva

--- ejercicios-DOM/ejercicio_4/index.js
+++ ejercicios-DOM/ejercicio_4/index.js
@@ -1,5 +1,24 @@
 // 1.1 Añade un botón a tu html con el id btnToClick y en tu javascript añade el evento click que ejecute un console log con la información del evento del click
 
+const Click = document.getElementById("btnToClick");
+
+Click.addEventListener("click", function(evento) {
+    console.log("Se ha pulsado el botón");
+    console.log(evento); 
+});
+
 // 1.2 Añade un evento 'focus' que ejecute un console.log con el valor del input.
 
+const eventofocus = document.querySelector(".focus");
+
+eventofocus.addEventListener("focus", function(evento2) {
+    console.log(evento2.target.value);
+});
+
 // 1.3 Añade un evento 'input' que ejecute un console.log con el valor del input.
+
+const eventovalue = document.querySelector(".value");
+
+eventovalue.addEventListener("input", (evento3) => {
+    console.log(evento3.target.value);
+});
--- ejercicios-DOM/ejercicio_5/index.js
+++ ejercicios-DOM/ejercicio_5/index.js
@@ -2,9 +2,42 @@
 //que imprima cada uno de los albums. Que tenga la apariencia de una web completa dentro de lo posible.
 
 const albums = [
-  "De Mysteriis Dom Sathanas",
-  "Reign of Blood",
-  "Ride the Lightning",
-  "Painkiller",
-  "Iron Fist",
+  { title: "De Mysteriis Dom Sathanas", image: "fotos/Demysteriis.jpg", link: "https://open.spotify.com/intl-es/album/7mHVfSXtdzvPk7TkdhoekE" },
+  { title: "Reign of Blood", image: "fotos/reignofblood.jpg", link: "https://open.spotify.com/intl-es/album/5v5BfkxWDAKTkzrXl3H0mU"},
+  { title: "Ride the Lightning", image: "fotos/ridethelightning.jpg", link: "https://open.spotify.com/intl-es/album/05DePtm7oQMdL3Uzw2Jmsc"},
+  { title: "Painkiller", image: "fotos/painkiller.jpg", link: "https://open.spotify.com/intl-es/album/7LgrhuKnAXpNEv8qzcVd2t"},
+  { title: "Iron Fist", image: "fotos/ironfist.jpg", link: "https://open.spotify.com/intl-es/album/728HEEgXa98wouqC8nOdQR"},
 ];
+
+const ul = document.createElement("ul");
+
+for (let i = 0; i < albums.length; i++) {
+  const li = document.createElement("li");
+
+  const title = document.createElement("p");
+  title.textContent = albums[i].title;
+  li.appendChild(title);
+
+  const img = document.createElement("img");
+  img.src = albums[i].image;
+  img.alt = albums[i].title;
+  img.style.width = "150px";
+  li.appendChild(img);
+
+  const button = document.createElement("button");
+  button.textContent = "Escuchar album en Spotify";
+
+   button.addEventListener("click", () => {
+    if (albums[i].link) {
+      window.open(albums[i].link, "_blank");
+    } else {
+      alert("Link no disponible en Spoty");
+    }
+  });
+
+
+  li.appendChild(button);
+  ul.appendChild(li);
+}
+
+document.body.appendChild(ul);
